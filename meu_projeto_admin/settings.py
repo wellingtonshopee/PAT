e74996f2131cea466bd980dbf2bf84dee7a7517d
wellingtonshopee <wellingtonshopeemuriae@outlook.com>
2025-07-03 10:43:39 -0300

    Update settings.py

--- meu_projeto_admin/settings.py
+++ meu_projeto_admin/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # Mantenha como False para produção
+DEBUG = True # Mantenha como False para produção
 
 # ALtere para a URL do seu Render e adicione localhost para desenvolvimento
 ALLOWED_HOSTS = ['pat-gmhm.onrender.com', 'localhost', '127.0.0.1']
